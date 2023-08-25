ec16ecb3f662eb8b67ca84491e21cc32ae2cf3d5
Victoria Zhuravleva <Zhuravleva987@mail.ru>
2023-08-25 16:30:01 +0400

    Задание 7

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,9 +1,15 @@
 const form = document.querySelector("#form")
 const progress = document.querySelector("#progress")
 
-form.addEventListener("submit", function () {
+form.addEventListener("submit", function (event) {
+  event.preventDefault()
   const formSent = new FormData(form)
   const xhr = new XMLHttpRequest()
+
+  xhr.upload.onprogress = function (event) {
+    const progress = document.getElementById("progress")
+    progress.value = event.loaded / event.total
+  }
 
   xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload")
   let response = xhr.response
